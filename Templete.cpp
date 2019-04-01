9f6564b9046e2657037956004d4ff7c8eeceb6ab
RyutaKremer <r_kremer@keio.jp>
2019-04-01 22:46:06 +0900

    add RREPALL and Yes/No output function

--- Templete.cpp
+++ Templete.cpp
@@ -5,10 +5,14 @@ using namespace std;
 #define RFOR(i,a,b) for (int i = a-1; i >= b; i--)
 #define RREP(i,a) RFOR(i,a,0)
 #define REPALL(i,v) for (int i = 0; i < v.size(); i++)
+#define RREPALL(i,v) for (int i = v.size()-1; i >= 0; i--)
 #define SORT(v) sort(v.begin(), v.end())
 #define MIN_ELEMENT(v) min_element(v.begin(), v.end())
 #define MAX_ELEMENT(v) max_element(v.begin(), v.end())
 #define COUNT(v,n) count(v.begin(), v.end(), n);
+void YES(bool flag) {cout<<(flag ? "YES" : "NO")<<endl;}
+void Yes(bool flag) {cout<<(flag ? "Yes" : "No")<<endl;}
+void yes(bool flag) {cout<<(flag ? "yes" : "no")<<endl;}
 typedef long long ll;
 typedef unsigned long long ull;
 const int INF = 1e7;
